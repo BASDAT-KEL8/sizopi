206e108ef6b38665938ec74d841f8b6b86b267f1
Ignasius Bramantya Widiprasetya <ignasius.bramantya@gmail.com>
2025-04-27 23:29:57 +0700

    feat: auth

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('register/', views.choose_role_view, name='choose_role'),  # Pilih role dulu
+    path('register/pengunjung/', views.register_pengunjung_view, name='register_pengunjung'),
+    path('register/dokter/', views.register_dokter_view, name='register_dokter'),
+    path('register/staff/', views.register_staff_view, name='register_staff'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('register/', views.register_view, name='register'),
-    path('profile/', views.profile_view, name='profile'),
+
+    path('', lambda request: redirect('/accounts/login/')),
+
 ]
---
+++ dashboard/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+
+def dashboard_view(request):
+    if 'email' not in request.session:
+        return redirect('login')
+
+    nama_lengkap = request.session.get('nama_lengkap', 'User')
+    return render(request, 'dashboard/dashboard.html', {'nama_lengkap': nama_lengkap})
