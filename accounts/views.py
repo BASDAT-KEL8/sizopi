e4bf74dcc225a99e632699106ef97c209bb206ed
shintia_dharma <shintia.ds@icloud.com>
2025-04-28 23:25:59 +0700

    crud rekam medis using dummy

--- accounts/views.py
+++ accounts/views.py
@@ -143,20 +143,25 @@ def logout_view(request):
 def choose_role_view(request):
     return render(request, 'accounts/choose_role.html')
 
-def navbar_view(request):
-    user_role = "guest"
-    if request.user.is_authenticated:
-        username = request.user.username
-        if DokterHewan.objects.filter(username_dh=username).exists():
-            user_role = "dokter"
-        elif PenjagaHewan.objects.filter(username_ph=username).exists():
-            user_role = "penjaga"
-        elif StafAdmin.objects.filter(username_sa=username).exists():
-            user_role = "admin"
-        elif PelatihHewan.objects.filter(username_lh=username).exists():
-            user_role = "pelatih"
-        elif Adopter.objects.filter(username_adopter=username).exists():
-            user_role = "pengunjung_adopter"
-        else:
-            user_role = "pengunjung"
-    return render(request, 'navbar.html', {'user_role': user_role})
+# def navbar_view(request):
+#     user_role = "guest"
+#     if request.user.is_authenticated:
+#         username = request.user.username
+#         if DokterHewan.objects.filter(username_dh=username).exists():
+#             user_role = "dokter"
+#         elif PenjagaHewan.objects.filter(username_ph=username).exists():
+#             user_role = "penjaga"
+#         elif StafAdmin.objects.filter(username_sa=username).exists():
+#             user_role = "admin"
+#         elif PelatihHewan.objects.filter(username_lh=username).exists():
+#             user_role = "pelatih"
+#         elif Adopter.objects.filter(username_adopter=username).exists():
+#             user_role = "pengunjung_adopter"
+#         else:
+#             user_role = "pengunjung"
+#     return render(request, 'accounts/navbar.html', {'user_role': user_role})
+
+#testing
+def navbar_view(request): 
+    user_role = request.GET.get('role', 'guest')
+    return render(request, 'accounts/navbar.html', {'user_role': user_role})
