d56f3d7c889b9f34ac86a3431e661e7bf04b8a84
shintia_dharma <shintia.ds@icloud.com>
2025-04-29 22:03:29 +0700

    crud penjadwalan pemeriksaan kesehatan

--- penjadwalan/urls.py
+++ penjadwalan/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'penjadwalan'
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('jadwal-pemeriksaan/', views.list_jadwal_pemeriksaan, name='list_jadwal_pemeriksaan'),
+    path('jadwal-pemeriksaan/<uuid:id_hewan>/tambah/', views.tambah_jadwal_pemeriksaan, name='tambah_jadwal_pemeriksaan'),
+    path('jadwal-pemeriksaan/<uuid:id_hewan>/<str:jadwal_id>/edit/', views.edit_jadwal_pemeriksaan, name='edit_jadwal_pemeriksaan'),
+    path('jadwal-pemeriksaan/<uuid:id_hewan>/edit-frekuensi/', views.edit_frekuensi_pemeriksaan, name='edit_frekuensi_pemeriksaan'),
+    path('jadwal-pemeriksaan/<uuid:id_hewan>/<str:jadwal_id>/hapus/', views.hapus_jadwal_pemeriksaan, name='hapus_jadwal_pemeriksaan'),
 ]
